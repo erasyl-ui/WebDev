091129f55e54032edb3bef8242b2035d5970c2a0
erasyl-ui <yelubeky@mail.ru>
2026-03-17 13:39:27 +0500

    Create max_end3.py

---
+++ lab7/task1/codingBat/List-1/max_end3.py
@@ -0,0 +1,5 @@
+def max_end3(nums):
+  nums_length = len(nums)
+  max_element = nums[0] if (nums[0] > nums[nums_length-1]) else nums[nums_length-1]
+
+  return [max_element, max_element, max_element]
