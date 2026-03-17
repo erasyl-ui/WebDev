f0616645708939a5e62297329406e52b1c002dbf
erasyl-ui <yelubeky@mail.ru>
2026-03-17 13:31:38 +0500

    Add array123 function to check for sequence
    Implement array123 function to check for sequence 1, 2, 3 in a list.

---
+++ lab7/task1/codingBat/Warmup-2/array123.py
@@ -0,0 +1,8 @@
+def array123(nums):
+  
+  for idx, elem in enumerate(nums):
+    if (elem == 1):
+      if ((len(nums) > (idx + 2)) and (nums[idx+1] == 2) and (nums[idx+2] == 3)):
+        return True
+        
+  return False
